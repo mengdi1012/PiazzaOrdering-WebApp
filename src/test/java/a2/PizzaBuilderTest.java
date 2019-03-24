24b5792de85b34350c404aaef7fc00e85068b71a
Jayso <jaysonpan11@gmail.com>
2019-03-24 23:37:54 +0800

    add test for pizzza builder

--- src/test/java/a2/PizzaBuilderTest.java
+++ src/test/java/a2/PizzaBuilderTest.java
@@ -10,8 +10,13 @@ import static org.junit.Assert.*;
 public class PizzaBuilderTest {
     @Test
     public void testPizzaCreation() throws Exception {
+        PizzaType type = PizzaType.PEPPERONI;
+        PizzaSize size = PizzaSize.SMALL;
         List<Topping> toppings = new ArrayList<Topping>();
         toppings.add(Topping.OLIVES);
-        Pizza newPizza = new PizzaBuilder().type(PizzaType.PEPPERONI.toString()).size(PizzaSize.SMALL).toppings(toppings).build();
+        Pizza newPizza = new PizzaBuilder().type(type.toString()).size(size).toppings(toppings).build();
+        assertEquals(newPizza.getType(), type);
+        assertEquals(newPizza.getSize(), size);
+        assertEquals(newPizza.getExtraToppings(), toppings);
     }
 }
